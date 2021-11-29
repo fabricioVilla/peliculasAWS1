c18532f258ee0766b01bf02b79f97a18b0eae63e
Angel Fabricio Perez Villanueva <afperez@frd.org.mx>
2021-11-29 11:59:35 -0600

    Primera actualizacion

---
+++ objetos/peliculas.py
@@ -0,0 +1,46 @@
+
+Peliculas=[
+    {
+    'nompre': "mad max",
+    'generos': ["Accion", "Ficcion"],
+    'recena': "esta buena",
+    'duracion': 60,
+    'en_exposicon': False,
+    'fecha_de_estreno': "04/07/2017",
+    },
+    {
+    'nompre': "vierenes 13",
+    'generos': ["Terror", "Ficcion"],
+    'recena': "esta mas o menos",
+    'duracion': 90,
+    'en_exposicon': True,
+    'fecha_de_estreno': "04/07/1997",
+    },
+    {
+    'nompre': "robocop",
+    'generos': ["Accion", "Ficcion", "policia"],
+    'recena': "esta increible",
+    'duracion': 60,
+    'en_exposicon': False,
+    'fecha_de_estreno': "04/07/1998",
+    },
+    {
+    'nompre': "ted",
+    'generos': ["Comedia"],
+    'recena': "esta chiatosa",
+    'duracion': 60,
+    'en_exposicon': False,
+    'fecha_de_estreno': "04/07/2012",
+    },
+     {
+    'nompre': "ted2",
+    'generos': ["Comedia"],
+    'recena': "esta chiatosa",
+    'duracion': 60,
+    'en_exposicon': False,
+    'fecha_de_estreno': "04/07/2020",
+    }
+
+]
+
+
